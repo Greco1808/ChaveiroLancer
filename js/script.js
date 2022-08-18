aeb6f69db0b2beab3f15f97a1a59aa938e8e55f1
Celso Muniz Greco Filho <87503654+Greco1808@users.noreply.github.com>
2022-08-18 07:57:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,66 +1,110 @@
-var index =0
-var n;
+//var index =0
+/var n;
 
 
-console.log(slides)
+//console.log(slides)
 
-function avanca(n){
+//function avanca(n){
  
-  controlaSlide(index += n)
+  //controlaSlide(index += n)
   
-}
+//}
 
-function avanca2(n){
+//function avanca2(n){
  
-  controlaSlide2(index += n)
+  //controlaSlide2(index += n)
   
-}
+//}
 
 
-function controlaSlide(){
+//function controlaSlide(){
     
 
 
-  var slides = document.getElementsByClassName("item")
+  //var slides = document.getElementsByClassName("item")
   
   
   
   
-  if(index >= slides.length){
-    index = 0;
-  }
+  //if(index >= slides.length){
+    //index = 0;
+  //}
   
-  if(index < 0){
-    index = slides.length - 1;
-  }
+  //if(index < 0){
+    //index = slides.length - 1;
+ //}
   
- var count = slides[index]
+ //var count = slides[index]
    
  
- count.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+ //count.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
   
-}
+//}
 
-function controlaSlide2(){
+//function controlaSlide2(){
     
 
 
-  var slides2 = document.getElementsByClassName("item2")
+  //var slides2 = document.getElementsByClassName("item2")
   
   
   
   
-  if(index >= slides2.length){
-    index = 0;
+  //if(index >= slides2.length){
+    //index = 0;
+  //}
+  
+  //if(index < 0){
+    //index = slides2.length - 1;
+  //}
+  
+ //var count = slides2[index]
+   
+ 
+ //count.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+  
+//}
+
+var indice =0
+var y;
+var classe;
+
+
+function avancaSnap(y,classe){
+ 
+  controlaSnap(indice += y,classe)
+    //controlaSlide(classe)
+    console.log(classe)
+    console.log(y)
+  
+  
+  
+}
+
+
+
+
+function controlaSnap(y,classe){
+    
+
+
+  var slides = document.getElementsByClassName(classe)
+  
+  
+  
+  
+  if(indice >= slides.length){
+    indice = 0;
   }
   
-  if(index < 0){
-    index = slides2.length - 1;
+  if(indice < 0){
+    indice = slides.length - 1;
   }
   
- var count = slides2[index]
+ var count = slides[indice]
+ console.log(slides)
    
  
  count.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
-  
+  console.log(slides)
 }
